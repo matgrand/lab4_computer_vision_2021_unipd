f1ba88a1866590079e6e4f5a3450d333523c5c06
grandin <matteo.grandin@gmail.com>
2021-04-28 13:10:41 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+int main(void)
+{
+	Mat img(200, 200, CV_8U);
+	for (int i = 0; i < 200; ++i)
+		for (int j = 0; j < 200; ++j)
+			img.at<unsigned char>(i, j) = std::min(i + j, 255);
+	namedWindow("Example 2");
+	imshow("Example 2", img);
+	waitKey(0);
+	return 0;
+}
